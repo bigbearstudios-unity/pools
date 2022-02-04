ebc173e102084ef9a1dab451d3c449b2a77ae5bf
Stuart Farnaby <stuart@bigbearstudios.co.uk>
2022-02-04 21:24:04 +0000

    - Added Spawn methods for the StaticPoolDefinition

--- Runtime/StaticPoolDefinition.cs
+++ Runtime/StaticPoolDefinition.cs
@@ -13,5 +13,69 @@ namespace BBUnity {
         public StaticPoolDefinition(string name, GameObject prefab, int defaultSize) : base(name, prefab, defaultSize) {}
         public StaticPoolDefinition(GameObject prefab, int defaultSize, int maximumSize) : base(prefab, defaultSize, maximumSize) { }
         public StaticPoolDefinition(string name, GameObject prefab, int defaultSize, int maximumSize) : base(name, prefab, defaultSize, maximumSize) {}
+
+        public PoolBehaviour Spawn(string definitionName, Vector3 position) {
+            PoolBehaviour poolBehaviour = _Spawn();
+            if(poolBehaviour != null) {
+                poolBehaviour.SetPosition(position);
+                poolBehaviour._OnSpawn();
+            }
+
+            return null;
+        }
+
+        public PoolBehaviour Spawn(string definitionName, Vector3 position, Vector3 scale) {
+            PoolBehaviour poolBehaviour = _Spawn();
+            if(poolBehaviour != null) {
+                poolBehaviour.SetPosition(position);
+                poolBehaviour.SetLocalScale(scale);
+                poolBehaviour._OnSpawn();
+            }
+
+            return null;
+        }
+
+        public PoolBehaviour Spawn(string definitionName, Transform parent) {
+            PoolBehaviour poolBehaviour = _Spawn();
+            if(poolBehaviour != null) {
+                poolBehaviour.SetParent(parent);
+                poolBehaviour._OnSpawn();
+            }
+
+            return null;
+        }
+
+        public PoolBehaviour Spawn(string definitionName, Transform parent, Vector3 position, Vector3 scale) {
+            PoolBehaviour poolBehaviour = _Spawn();
+            if(poolBehaviour != null) {
+                poolBehaviour.SetParent(parent);
+                poolBehaviour.SetPosition(position);
+                poolBehaviour.SetLocalScale(scale);
+                poolBehaviour._OnSpawn();
+            }
+
+            return null;
+        }
+
+        public PoolBehaviour Spawn(string definitionName, System.Action<PoolBehaviour> beforeSpawn) {
+            PoolBehaviour poolBehaviour = _Spawn();
+            if(poolBehaviour != null) {
+                beforeSpawn(poolBehaviour);
+                poolBehaviour._OnSpawn();
+            }
+
+            return null;
+        }
+
+        public PoolBehaviour Spawn(string definitionName, System.Action<PoolBehaviour> beforeSpawn, System.Action<PoolBehaviour> afterSpawn) {
+            PoolBehaviour poolBehaviour = _Spawn();
+            if(poolBehaviour != null) {
+                beforeSpawn(poolBehaviour);
+                poolBehaviour._OnSpawn();
+                afterSpawn(poolBehaviour);
+            }
+
+            return null;
+        }
     }
 }
